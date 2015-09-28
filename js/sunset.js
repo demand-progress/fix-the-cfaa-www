dc92db8857715dccbc808a54b17cbbaffedddc30
Chris Antaki <ChrisAntaki@gmail.com>
2015-09-28 16:09:47 -0700

    Updated share text & tweaked a few colors

--- js/sunset.js
+++ js/sunset.js
@@ -199,7 +199,7 @@ var fb = document.querySelectorAll('a.facebook');
 for (var i = 0; i < fb.length; i++) {
     fb[i].addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fnetneutralitybrief.com%2F%3Fref%3D' + org.id);
+        window.open('https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Ffixthecfaa.com');
     }, false);
 }
 
@@ -207,7 +207,7 @@ var tws = document.querySelectorAll('a.twitter');
 for (var i = 0; i < tws.length; i++) {
     tws[i].addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(TWEET_TEXT) + org.id);
+        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(TWEET_TEXT));
     }, false);
 }
 
@@ -215,7 +215,7 @@ var ems = document.querySelectorAll('a.email');
 for (var i = 0; i < ems.length; i++) {
     ems[i].addEventListener('click', function(e) {
         e.preventDefault();
-        window.location.href = 'mailto:?subject=' + encodeURIComponent(EMAIL_SUBJECT) + '&body=http%3A%2F%2Fnetneutralitybrief.com%2F';
+        window.location.href = 'mailto:?subject=' + encodeURIComponent(EMAIL_SUBJECT) + '&body=http%3A%2F%2Ffixthecfaa.com%2F';
     }, false);
 }
 
